ca1a2c1777b79804a3b3e4ecf9d4fa7dbe1f2e78
RaulCT <raul.canul@springlabs.net>
2019-02-07 11:13:15 -0600

    Data mockup changed on PersonList component

--- src/containers/RegisteredPersonsList.js
+++ src/containers/RegisteredPersonsList.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
 
 import NavigationBar from '../components/RegisteredPersonsList/AppBar'
+import PersonsList from '../components/RegisteredPersonsList/PersonsList'
+import { withStyles } from '@material-ui/core';
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
+
+const styles = theme => ( {
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 2
+  }
+} )
 
 class RegisteredPersonsList extends Component {
 
   render() {
+    const { classes } = this.props
+
     return(
       <div>
         <NavigationBar />
-        Registered Person List
+
+        <PersonsList />
+
+        <Fab className={classes.fab} color="secondary">
+          <AddIcon />
+        </Fab>
       </div>
     )
   }
 }
 
-export default RegisteredPersonsList
+export default withStyles( styles )( RegisteredPersonsList )
